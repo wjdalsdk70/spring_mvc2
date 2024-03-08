75e0b9ab87cba80ac995e1e4c47fe9187db13a73
이정민 <kyungheeyj45@ajou.ac.kr>
2024-03-08 19:28:43 +0900

    섹션9 : API 예외 처리 - 시작

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,10 +1,17 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -12,16 +19,16 @@ public class ErrorPageController {
 
     //RequestDispatcher 상수로 정의되어 있음
     public static final String ERROR_EXCEPTION =
-            "javax.servlet.error.exception";
+            "jakarta.servlet.error.exception";
     public static final String ERROR_EXCEPTION_TYPE =
-            "javax.servlet.error.exception_type";
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
+            "jakarta.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
     public static final String ERROR_REQUEST_URI =
-            "javax.servlet.error.request_uri";
+            "jakarta.servlet.error.request_uri";
     public static final String ERROR_SERVLET_NAME =
-            "javax.servlet.error.servlet_name";
+            "jakarta.servlet.error.servlet_name";
     public static final String ERROR_STATUS_CODE =
-            "javax.servlet.error.status_code";
+            "jakarta.servlet.error.status_code";
 
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse
@@ -37,6 +44,20 @@ public class ErrorPageController {
         log.info("errorPage 500");
         printErrorInfo(request);
         return "error-page/500";
+    }
+
+    @RequestMapping(value = "/error-page/500",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String,Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response)
+    {
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
     }
 
     private void printErrorInfo(HttpServletRequest request) {
